b7727a6b6968514445aec46f4b3c6729dc833e69
AnnKangHo <duddnd6897@gmail.com>
2022-10-12 23:48:36 +0900

    practice 1

---
+++ project/Model/model.swift
@@ -0,0 +1,8 @@
+//
+//  model.swift
+//  project
+//
+//  Created by AnnKangHo on 2022/10/12.
+//
+
+import Foundation
--- project/ViewController.swift
+++ project/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LBtext: UILabel!
+    @IBOutlet weak var TFname: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.view.backgroundColor = .red
+        LBtext.text = TFname.text! + " 님,\n\n 안녕하세 :)"
     }
 
 
