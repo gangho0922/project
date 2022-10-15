4877e79ccdc29171961c2549f1af057fb358dec1
AnnKangHo <duddnd6897@gmail.com>
2022-10-15 22:34:35 +0900

    로그인 화면 맨윗 부분

--- project/ViewController.swift
+++ project/ViewController.swift
@@ -8,16 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mainLabel = UILabel()
+    var emailField = UITextField()
+    var passwordField = UITextField()
+    var loginButton = UIButton()
+    var backgroundImage = UIImageView()
+    var profileImage = UIImageView()
+    var profileImagePicker = UIImagePickerController()
+    var backgroundImagePicker = UIImagePickerController()
 
-    @IBOutlet weak var LBtext: UILabel!
-    @IBOutlet weak var TFname: UITextField!
+    // MARK: LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .red
-        LBtext.text = TFname.text! + " 님,\n\n 안녕하세 :)"
+        self.view.backgroundColor = .white
+        self.view.addSubview(mainLabel)
+        self.view.addSubview(emailField)
+        self.view.addSubview(passwordField)
+        self.view.addSubview(loginButton)
+        self.view.addSubview(backgroundImage)
+        self.view.addSubview(profileImage)
+
+        self.mainLableLayout()
+        self.emailFieldLayout()
+        self.passwordFieldLayout()
+        self.loginButtonLayout()
+        self.backgroundImageLayout()
+        self.profileImageLayout()
+        self.tapImage(imageView: backgroundImage)
+        self.tapImage(imageView: profileImage)
+
+        //self.loginButton.addTarget(self, action: #selector(self.login), for: .touchUpInside)
+
+        self.title = "로그인"
     }
 
+    func mainLableLayout(){
 
+    }
+    func emailFieldLayout(){
+
+    }
+    func passwordFieldLayout(){
+
+    }
+    func loginButtonLayout(){
+
+    }
+    func backgroundImageLayout(){
+    }
+    func profileImageLayout(){
+    }
+    func tapImage(imageView: UIImageView){
+    }
 }
 
