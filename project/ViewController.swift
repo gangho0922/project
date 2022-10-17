a4bbba83ed924ca9f97633e6c09b1d0995e1ce33
AnnKangHo <duddnd6897@gmail.com>
2022-10-17 20:06:13 +0900

    회원가입 1

--- project/ViewController.swift
+++ project/ViewController.swift
@@ -13,21 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        self.title = "로그인"
-        let textView = UITextView()
-        textView.backgroundColor = .white
-        textView.layer.borderWidth = 1.0
-        textView.font = size 
-        textView.layer.borderColor = UIColor.black.cgColor
-        view.addSubview(textView)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        textView.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        textView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        textView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        textView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 50).isActive = true;
-        textView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -50).isActive = true;
+        self.title = "회원가입"
+        let text1View = UITextView()
+        text1View.backgroundColor = .white
+            text1View.layer.borderWidth = 1.0
+            text1View.font = UIFont.systemFont(ofSize: CGFloat(20))
+            text1View.layer.borderColor = UIColor.black.cgColor
+            view.addSubview(text1View)
+            text1View.translatesAutoresizingMaskIntoConstraints = false
+            text1View.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            text1View.heightAnchor.constraint(equalToConstant: 50).isActive = true;
+            text1View.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            text1View.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            text1View.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 60).isActive = true;
+            text1View.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -60).isActive = true;
+        let text2view = UITextView()
+        text1View.backgroundColor = .white
+            text1View.layer.borderWidth = 1.0
+            text1View.font = UIFont.systemFont(ofSize: CGFloat(20))
+            text1View.layer.borderColor = UIColor.black.cgColor
+            view.addSubview(text1View)
+            text1View.translatesAutoresizingMaskIntoConstraints = false
+            text1View.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            text1View.heightAnchor.constraint(equalToConstant: 50).isActive = true;
+            text1View.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            text1View.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            text1View.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 60).isActive = true;
+            text1View.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -60).isActive = true;
     }
 }
 
