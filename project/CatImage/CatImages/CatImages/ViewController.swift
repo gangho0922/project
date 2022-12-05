008b9fca1ab93247b41bf30617b3dd1eebdc2b69
AnnKangHo <duddnd6897@gmail.com>
2022-12-05 17:15:03 +0900

    Image setting

--- project/CatImage/CatImages/CatImages/ViewController.swift
+++ project/CatImage/CatImages/CatImages/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        return cv
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.SetupView()
+    }
+    private func SetupView(){
+        self.view.addSubview(self.collectionView)
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        self.collectionView.backgroundColor = .red
+        self.collectionView.register(UICollectionViewCell.self,
+                                     forCellWithReuseIdentifier: "Cell")
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.reloadData()
     }
 
-
 }
-
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
+}
